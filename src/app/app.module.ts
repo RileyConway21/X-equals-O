ed2300fb015a996e26c7297fec2e0fd43a74bd4f
RileyConway21 <Rileyconway363@gmail.com>
2022-11-03 14:12:40 -0500

    X-equals-O

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
